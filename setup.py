090ba75ddf4df9dbda129ac38d40e5c782757e8c
tdpetrou <petrou.theodore@gmail.com>
2020-04-27 18:03:15 -0400

    0.0.5

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ long_description = re.sub(pat, repl, long_description)
 
 setuptools.setup(
     name="bar_chart_race",
-    version="0.0.4",
+    version="0.0.5",
     author="Ted Petrou",
     author_email="petrou.theodore@gmail.com",
-    description="Creates animated bar chart races using matplotlib",
+    description="Create animated bar chart races using matplotlib",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dexplo/bar_chart_race",
