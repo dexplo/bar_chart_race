0a133a650c0f4591ceb59504011df9da6651964b
tdpetrou <petrou.theodore@gmail.com>
2020-05-12 09:48:40 -0400

    license update

--- setup.py
+++ setup.py
@@ -10,17 +10,22 @@ long_description = re.sub(pat, repl, long_description)
 
 setuptools.setup(
     name="bar_chart_race",
-    version="0.0.6",
+    version="0.0.7",
     author="Ted Petrou",
     author_email="petrou.theodore@gmail.com",
     description="Create animated bar chart races using matplotlib",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    keywords="visualization animation bar chart race matplotlib pandas",
     url="https://github.com/dexplo/bar_chart_race",
     packages=setuptools.find_packages(),
+    license='MIT',
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: BSD License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=["pandas>=0.24", "matplotlib>=3.1"],
+    python_requires='>=3.6',
+    include_package_data=True,
 )
