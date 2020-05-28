fae207e03409d5a02489c8821b18ad7ca1615c51
tdpetrou <petrou.theodore@gmail.com>
2020-05-28 19:04:53 -0400

    __all__ for pdoc

--- bar_chart_race/__init__.py
+++ bar_chart_race/__init__.py
@@ -1,2 +1,3 @@
 from ._make_chart import bar_chart_race, load_dataset, prepare_wide_data, prepare_long_data
 __version__ = "0.0.7"
+__all__ = ['bar_chart_race', 'load_dataset', 'prepare_wide_data', 'prepare_long_data']
