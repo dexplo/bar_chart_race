252335f211d034948cc6a7728da91c0659423b8b
tdpetrou <petrou.theodore@gmail.com>
2020-05-29 14:24:18 -0400

    use img

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from bar_chart_race import __version__
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-pat = r'!\[gif\]\('
-repl = r'![gif](https://raw.githubusercontent.com/dexplo/bar_chart_race/master/'
+pat = r'!\[img\]\('
+repl = r'![img](https://raw.githubusercontent.com/dexplo/bar_chart_race/master/'
 long_description = re.sub(pat, repl, long_description)
 
 setuptools.setup(
