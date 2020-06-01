0bb2893f802679cee886b18160b8fbfd4b084f81
tdpetrou <petrou.theodore@gmail.com>
2020-06-01 11:34:55 -0400

    0.0.9

--- bar_chart_race/__init__.py
+++ bar_chart_race/__init__.py
@@ -1,3 +1,3 @@
 from ._make_chart import bar_chart_race, load_dataset, prepare_wide_data, prepare_long_data
-__version__ = "0.0.8"
+__version__ = "0.0.9"
 __all__ = ['bar_chart_race', 'load_dataset', 'prepare_wide_data', 'prepare_long_data']
