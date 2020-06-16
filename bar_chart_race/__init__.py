15a028b46cc5184671058670204b15471c055a20
tdpetrou <petrou.theodore@gmail.com>
2020-06-16 08:54:17 -0400

    new version reading

--- bar_chart_race/__init__.py
+++ bar_chart_race/__init__.py
@@ -1,5 +1,6 @@
 from ._make_chart import bar_chart_race, load_dataset, prepare_wide_data, prepare_long_data
 from ._make_chart_plotly import bar_chart_race_plotly
-__version__ = "0.2.0"
+
+__version__ = '0.2.0'
 __all__ = ['bar_chart_race', 'bar_chart_race_plotly',
            'load_dataset', 'prepare_wide_data', 'prepare_long_data']
--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 import setuptools
 import re
-from bar_chart_race import __version__
+
+with open('bar_chart_race/__init__.py', 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.split("'")[1]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -11,13 +15,13 @@ long_description = re.sub(pat, repl, long_description)
 
 setuptools.setup(
     name="bar_chart_race",
-    version=__version__,
+    version=version,
     author="Ted Petrou",
     author_email="petrou.theodore@gmail.com",
-    description="Create animated bar chart races using matplotlib",
+    description="Create animated bar chart races using matplotlib or plotly",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    keywords="visualization animation bar chart race matplotlib pandas",
+    keywords="visualization animation bar chart race matplotlib pandas plotly",
     url="https://github.com/dexplo/bar_chart_race",
     packages=setuptools.find_packages(),
     license='MIT',
