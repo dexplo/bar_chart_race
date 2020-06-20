5f8168e3047bcbf1efcc941101930a5076f29824
tdpetrou <petrou.theodore@gmail.com>
2020-06-20 09:12:25 -0400

    line chart

--- bar_chart_race/__init__.py
+++ bar_chart_race/__init__.py
@@ -1,6 +1,13 @@
-from ._make_chart import bar_chart_race, load_dataset, prepare_wide_data, prepare_long_data
+from ._make_chart import bar_chart_race
 from ._make_chart_plotly import bar_chart_race_plotly
+from ._line_chart_race import line_chart_race
+from ._utils import load_dataset, prepare_wide_data, prepare_long_data
 
 __version__ = '0.2.0'
-__all__ = ['bar_chart_race', 'bar_chart_race_plotly',
-           'load_dataset', 'prepare_wide_data', 'prepare_long_data']
+__all__ = [
+    'bar_chart_race', 
+    'bar_chart_race_plotly',
+    'load_dataset', 
+    'prepare_wide_data', 
+    'prepare_long_data',
+    'line_chart_race']
