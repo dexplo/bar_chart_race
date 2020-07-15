ca1562719e5948a88bc668a404aea10d78a18ed6
tdpetrou <petrou.theodore@gmail.com>
2020-07-15 13:39:47 -0400

    add plotly

--- bar_chart_race/_pandas_accessor.py
+++ bar_chart_race/_pandas_accessor.py
@@ -2,14 +2,15 @@ import re
 
 import pandas as pd
 
-from ._make_chart import bar_chart_race
+from ._make_chart import bar_chart_race as bcr
+from ._make_chart_plotly import bar_chart_race_plotly as bcrp
 
 
 @pd.api.extensions.register_dataframe_accessor("bcr")
 class _BCR:
 
-    def __init__(self, pandas_obj):
-        self._obj = pandas_obj
+    def __init__(self, df):
+        self._df = df
 
     def bar_chart_race(self, filename=None, orientation='h', sort='desc', n_bars=None, 
                        fixed_order=False, fixed_max=False, steps_per_period=10, 
@@ -21,12 +22,29 @@ class _BCR:
                        tick_label_size=7, shared_fontdict=None, scale='linear', writer=None, 
                        fig=None, dpi=144, bar_kwargs=None, filter_column_colors=False):
 
-        return bar_chart_race(self._obj, filename, orientation, sort, n_bars, fixed_order, fixed_max,
-                            steps_per_period, period_length, end_period_pause, interpolate_period, 
-                            bar_label_position, bar_label_fmt, bar_size, period_label, period_fmt, 
-                            period_summary_func, perpendicular_bar_func, figsize, cmap, title, 
-                            bar_label_size, tick_label_size, shared_fontdict, scale, writer, fig, 
-                            dpi, bar_kwargs, filter_column_colors)
+        return bcr(self._df, filename, orientation, sort, n_bars, fixed_order, fixed_max,
+                  steps_per_period, period_length, end_period_pause, interpolate_period, 
+                   bar_label_position, bar_label_fmt, bar_size, period_label, period_fmt, 
+                   period_summary_func, perpendicular_bar_func, figsize, cmap, title, 
+                   bar_label_size, tick_label_size, shared_fontdict, scale, writer, fig, 
+                   dpi, bar_kwargs, filter_column_colors)
 
-doc = bar_chart_race.__doc__
-_BCR.bar_chart_race.__doc__ = re.sub('df : .*(?=filename :)', '',  doc, flags=re.S)
+    def bar_chart_race_plotly(self, filename=None, orientation='h', sort='desc', n_bars=None, 
+                              fixed_order=False, fixed_max=False, steps_per_period=10, 
+                              period_length=500, interpolate_period=False, period_label=True, 
+                              period_fmt=None, period_summary_func=None, perpendicular_bar_func=None,
+                              colors=None, title=None, bar_size=.95, textposition='outside', 
+                              texttemplate=None, bar_label_font=12, tick_label_font=12, 
+                              hovertemplate=None, slider=True, scale='linear', bar_kwargs=None, 
+                              layout_kwargs=None, write_html_kwargs=None, filter_column_colors=False):
+
+        return bcrp(self._df, filename, orientation, sort, n_bars, fixed_order, fixed_max,
+                    steps_per_period, period_length, interpolate_period, period_label, 
+                    period_fmt, period_summary_func, perpendicular_bar_func, colors, title, 
+                    bar_size, textposition, texttemplate, bar_label_font, tick_label_font, 
+                    hovertemplate, slider, scale, bar_kwargs, layout_kwargs, write_html_kwargs,
+                    filter_column_colors)
+
+
+_BCR.bcr.__doc__ = re.sub('df : .*(?=filename :)', '',  bcr.__doc__, flags=re.S)
+_BCR.bcrp.__doc__ = re.sub('df : .*(?=filename :)', '',  bcrp.__doc__, flags=re.S)
