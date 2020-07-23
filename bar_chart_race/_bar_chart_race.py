ab80dc528889f0079517ce163d13e0d9ad2d03fd
tdpetrou <petrou.theodore@gmail.com>
2020-07-23 12:36:22 -0400

    example changed

--- bar_chart_race/_bar_chart_race.py
+++ bar_chart_race/_bar_chart_race.py
@@ -834,37 +834,37 @@ def bar_chart_race(df, filename=None, orientation='h', sort='desc', n_bars=None,
     df = bcr.load_dataset('covid19')
     bcr.bar_chart_race(
         df=df, 
-        filename='covid19_horiz_desc.mp4', 
+        filename='../docs/images/covid19_horiz.gif', 
         orientation='h', 
         sort='desc', 
         n_bars=8, 
         fixed_order=False, 
         fixed_max=True, 
-        steps_per_period=10, 
+        steps_per_period=20, 
         period_length=500, 
-        end_period_pause=300,
+        end_period_pause=0,
         interpolate_period=False, 
-        period_label={'x': .99, 'y': .8, 'ha': 'right', 'va': 'center'}, 
+        period_label={'x': .98, 'y': .3, 'ha': 'right', 'va': 'center'}, 
         period_template='%B %d, %Y', 
-        period_summary_func=lambda v, r: {'x': .85, 'y': .2, 
-                                          's': f'Total deaths: {v.sum()}', 
+        period_summary_func=lambda v, r: {'x': .98, 'y': .2, 
+                                          's': f'Total deaths: {v.sum():,.0f}', 
                                           'ha': 'right', 'size': 11}, 
         perpendicular_bar_func='median', 
         colors='dark12', 
         title='COVID-19 Deaths by Country', 
         bar_size=.95, 
         bar_textposition='inside',
-        bar_texttemplate=True, 
+        bar_texttemplate='{x:,.0f}', 
         bar_label_font=7, 
         tick_label_font=7, 
-        tick_template='{x:,.0f}'
-        shared_fontdict={'family' : 'Helvetica', 'weight' : 'bold', 'color' : '.1'}, 
+        tick_template='{x:,.0f}',
+        shared_fontdict=None, 
         scale='linear', 
         fig=None, 
         writer=None, 
         bar_kwargs={'alpha': .7},
-        fig_kwargs={'figsize': (6, 3.5), 'dpi': 144}
-        filter_column_colors=False)        
+        fig_kwargs={'figsize': (6, 3.5), 'dpi': 144},
+        filter_column_colors=False)
 
     Font Help
     ---------
